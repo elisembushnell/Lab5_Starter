b78b735652ad0122aa6d9f90cdb02a60b1eddb3c
elisembushnell <ebushnel@ucsd.edu>
2022-10-30 14:09:21 -0700

    Completed explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,45 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  populateVoices();
+  document.querySelector("button").addEventListener("click", playWords);
+}
+
+let voices = [];
+const synth = window.speechSynthesis;
+
+function populateVoices() {
+  const voiceSelect = document.querySelector('select');
+
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length ; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name}`;
+
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+function playWords() {
+  const inputTxt = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  var image = document.querySelector("[alt='Smiling face']");
+  
+  for (let i = 0; i < voices.length ; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+
+  
+  image.src = "assets/images/smiling-open.png";
+  synth.speak(utterThis);
+  image.src = "assets/images/smiling.png";
+
 }
